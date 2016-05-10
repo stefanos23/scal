51b54d0b918df0e0f4d5d570c7b48488e6438a28
Mario Preishuber <mario.preishuber@stud.sbg.ac.at>
2016-05-10 18:08:57 +0200

    Travis: Linux use gcc 4.8

--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -13,16 +13,11 @@ fi
 
 if [[ $(uname -s) = "Linux" ]]; then
   echo "Linux install start"
+  sudo apt-get install -qq gcc-4.8 g++-4.8 
   # We want to compile with g++ 4.8 when rather than the default g++
-  #sudo apt-get install -qq gcc-4.8 g++-4.8 
-  #sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
   
-  # alternative for the 2 line above
-  sudo update-alternatives --config gcc
-
-  # try to handle these pkgs via the .travis.yml file.
-  #sudo apt-get --fix-missing install google-perftools libgoogle-perftools-dev cmake libgtest-dev
-
+  sudo apt-get --fix-missing install google-perftools libgoogle-perftools-dev cmake libgtest-dev
   wget https://gflags.googlecode.com/files/libgflags0_2.0-1_amd64.deb
   sudo dpkg -i libgflags0_2.0-1_amd64.deb
   wget https://gflags.googlecode.com/files/libgflags-dev_2.0-1_amd64.deb
