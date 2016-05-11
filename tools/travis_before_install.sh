71e27650dd0f57b38174084d3bb69a3577c4cc28
Mario Preishuber <mario.preishuber@stud.sbg.ac.at>
2016-05-11 15:27:06 +0200

    Travis: Move required pkgs into .travis.yml

--- tools/travis_before_install.sh
+++ tools/travis_before_install.sh
@@ -2,15 +2,19 @@
 
 if [[ $(uname -s) = "Darwin" ]]; then
   echo "Darwin before install start"
-  brew update
-  brew outdated google-perftools || brew upgrade google-perftools
+  # brew update
+  # brew install google-perftools
+  # brew install gflags
+  # brew outdated google-perftools || brew upgrade google-perftools
+  # brew outdated gflags || brew upgrade gflags
   echo "Darwin before install end"
 fi
 
 if [[ $(uname -s) = "Linux" ]]; then
   echo "Linux before install start"
   # We need this to have g++4.8 available in apt
-  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  sudo apt-get update -qq
+  # sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  # sudo apt-get update -qq
+  # sudo apt-get install gcc-4.8 -y
   echo "Linux before install end"
 fi
