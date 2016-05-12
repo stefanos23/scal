8284fd9488831fd61878edbf26d9c5e205b78836
Mario Preishuber <mario.preishuber@stud.sbg.ac.at>
2016-05-12 18:31:48 +0200

    Travis: Set CXX, CC path.

--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -2,6 +2,8 @@
 
 if [[ $(uname -s) = "Darwin" ]]; then
   echo "Darwin install start"
+  echo ${CXX}
+  echo ${CC}
   
   # echo " -> install gflags"
   # git clone https://github.com/gflags/gflags.git
@@ -19,12 +21,9 @@ fi
 
 if [[ $(uname -s) = "Linux" ]]; then
   echo "Linux install start"
-  # We want to compile with g++ 4.8 when rather than the default g++
-  # sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
-  
+    
   echo " -> install gflags"
   sudo apt-get install --fix-missing google-perftools libgoogle-perftools-dev cmake libgtest-dev
-  # sudo apt-get install gflags
 
   wget https://gflags.googlecode.com/files/libgflags0_2.0-1_amd64.deb
   wget https://gflags.googlecode.com/files/libgflags-dev_2.0-1_amd64.deb
